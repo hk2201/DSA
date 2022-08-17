766d9f45ca4d0910b9ff615c2741ecb790ae01df
Harshad <harshadkhandare9000@gmail.com>
2022-08-17 22:30:48 +0530

    Kadens

---
+++ DSA Problems/ARRAYS/KadensAlgo.cpp
@@ -0,0 +1,51 @@
+// Find the max sum of the subarray in that array.
+
+//{ Driver Code Starts
+// Without changing the order.
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    long long maxSum(int arr[], int n)
+    {
+        int max = 0;
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+            if (sum > max)
+            {
+                max = sum;
+            }
+
+            if (sum < 0)
+            {
+                sum = 0;
+            }
+        }
+        return max;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    // your code goes here
+    int t;
+    cout << "Enter the size" << endl;
+    cin >> t;
+    int arr[t];
+    for (int i = 0; i < t; i++)
+    {
+        cout << "Enter the elemets" << endl;
+        cin >> arr[i];
+    }
+
+    Solution ob;
+    cout << ob.maxSum(arr, t);
+}
+
+// } Driver Code Ends
